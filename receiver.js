e112d6f945e7043376f03a381742a8c63ffa84e4
spgitaudio <98049014+spgitaudio@users.noreply.github.com>
2025-02-06 14:26:58 -0800

    add some debugging logs to playReceivedAudio

--- receiver.js
+++ receiver.js
@@ -2,10 +2,21 @@ let receivedAudio = document.getElementById("receivedAudio");
 let receivedStream;
 
 // 🎧 Play received WebRTC stream
+//function playReceivedAudio(stream) {
+//    receivedAudio.srcObject = stream;
+//    receivedStream = stream;
+//    console.log("🎙 Receiving & Playing WebRTC Audio...");
+//}
+
 function playReceivedAudio(stream) {
-    receivedAudio.srcObject = stream;
-    receivedStream = stream;
-    console.log("🎙 Receiving & Playing WebRTC Audio...");
+    let audioContext = new AudioContext();
+    let source = audioContext.createMediaStreamSource(stream);
+
+    console.log("🎙 WebRTC Received Stream:", stream);
+    console.log("🎚 WebRTC Audio Tracks:", stream.getAudioTracks().length);
+    console.log("🔍 Source Channel Count:", source.channelCount);
+
+    // source.connect(audioContext.destination);
 }
 
 // 🎙 Start recording received audio
